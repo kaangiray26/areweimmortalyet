b0f172e64d9ef4318da9712f3cfc5f2cc22b7f89
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2024-01-11 12:22:02 +0100

    init

---
+++ src/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="header row g-0 justify-content-center">
+        <div class="col-12 col-md-6">
+            <h1 class="mb-0">Not really, but we're getting there!</h1>
+            <p class="text-light opacity-50 mt-3 mb-0">A collection of materials about consciousness and the nature of
+                reality.
+            </p>
+        </div>
+    </div>
+    <div class="content row g-0 justify-content-center">
+        <div class="col-12 col-md-6">
+            <h2 class="title">Today</h2>
+            <p>
+                Alright, today we are actually not even that close to being done. But we are getting there! Some questions
+                need to be answered first, though. Like, what are we? Where are we located in the
+                body exactly? The brain? The heart? The gut? The whole body? Or maybe we are not even located in the body
+                at all? Maybe we are existing in a remote environment and just sharing the view, just like a <a
+                    href="https://en.wikipedia.org/wiki/Virtual_Network_Computing">VNC</a>?
+            </p>
+        </div>
+    </div>
+</template>
